a207a6357f6b55258b848e080c8aee3bc2b701c6
Alejandro Herrada <alejandropons17@gmail.com>
2017-08-27 02:31:11 -0400

    finished tasks 1 through 4

--- js/script.js
+++ js/script.js
@@ -1,5 +1,40 @@
-$(document).ready(function () {
-  $('div.square').mouseenter(function() {
-    $('this').toggleClass('full');
+function generateGrid(number) {
+  //holds the string to be appended to the #grid div
+  var divsToAppend = "";
+  //holds the total amount of squares (number squared)
+  var totalSquares = number * number;
+  //holds the side dimension for the square
+  var dimension = $('#grid').width();
+  //dimension should be the size of the #grid div divided by the amount of squares for each side
+  dimension /= number;
+  //round down to trim decimals while still ensuring squares will fit on grid
+  dimension = Math.floor(dimension);
+  //subtract two pixels to account for border perimeter
+  dimension -= 2;
+
+  for(var i = 0; i < totalSquares; i++) {
+    divsToAppend += '<div class="square" style="width: ' + dimension + 'px; height: ' + dimension + 'px;" />';
+  }
+  $('#grid').append(divsToAppend);
+}
+
+function clearGrid() {
+  var node = document.getElementById("grid");
+  node.innerHTML = '';
+}
+
+$(document).ready(function() {
+  $('.square').on("mouseenter", function() {
+    this.classList.add("mouseEnter");
+  });
+
+  generateGrid(12);
+
+  $('button').click(function() {
+    var input = parseInt(prompt("Enter a number"), 10);
+    if(input != null){
+      clearGrid();
+      generateGrid(input);
+    }
   });
 });
